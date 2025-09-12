aa6fae62c29d57a5256b6ed3f460040367f4c620
yadavravi1610 <ravinderyadav1610@gmail.com>
2025-09-13 01:20:37 +0530

    Whitespaces

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,12 @@
 # print("hello world");
 # print ("good morning");
 
-name = "john"
+# name = "john"
 
-if name == "john":
-    print(name)
+# if name == "john":
+#     print(name)
+
+x = 1 +   2   \
++3
+
+print(x)
