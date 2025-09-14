83ed3569af4f7879e2efcd9163e3dc6c593c9481
yadavravi1610 <ravinderyadav1610@gmail.com>
2025-09-15 00:30:15 +0530

    Loop reversal

--- for_loop.py
+++ for_loop.py
@@ -1,11 +1,22 @@
-n = int(input("Enter a positive integer: "))
+# n = int(input("Enter a positive integer: "))
 
-# Initialize sum
-total = 0
+# # Initialize sum
+# total = 0
 
-# Loop from 1 to n
-for i in range(1, n + 1):
-    total += i
+# # Loop from 1 to n
+# for i in range(1, n + 1):
+#     total += i
 
-# Print the result
-print(f"The sum of the first {n} natural numbers is: {total}")
+# # Print the result
+# print(f"The sum of the first {n} natural numbers is: {total}")
+
+# for i in range(2,8,3):
+#     print(i);
+
+n = int(input())
+
+# for i in range(n, 0, -2):
+# reverse of loop
+#      print(i)
+
+# print("Shivani")
