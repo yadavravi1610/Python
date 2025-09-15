7121d9c56d5767e269a978d8723909c5578477f7
yadavravi1610 <ravinderyadav1610@gmail.com>
2025-09-16 00:51:25 +0530

    Star pattern 2

--- patterns.py
+++ patterns.py
@@ -1,10 +1,20 @@
 
+# n=3
+
+# for i in range(1, n+1):
+#     for j in range(1, n+1):
+#         if j<=i:
+#             print("#", end="") 
+#         else:
+#             print("", end="")
+#     print("")   
+
 n=3
 
 for i in range(1, n+1):
     for j in range(1, n+1):
-        if j<=i:
-            print("#", end="") 
+        if j >= n+1- i:
+            print("*", end="") 
         else:
-            print("", end="")
-    print("")   
+            print(" ", end="")
+    print("") 
