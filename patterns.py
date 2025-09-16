00a16a58caac914171f2b53547ca3a13f32433d5
yadavravi1610 <ravinderyadav1610@gmail.com>
2025-09-16 14:30:35 +0530

    Conting pattern

--- patterns.py
+++ patterns.py
@@ -9,12 +9,23 @@
 #             print("", end="")
 #     print("")   
 
-n=3
+# n=3
 
+# for i in range(1, n+1):
+#     for j in range(1, n+1):
+#         if j >= n+1- i:
+#             print("*", end="") 
+#         else:
+#             print(" ", end="")
+#     print("") 
+
+n=4
+k=1
 for i in range(1, n+1):
     for j in range(1, n+1):
-        if j >= n+1- i:
-            print("*", end="") 
+        if j <= i:
+            print(k, end="")
+            k+=1 
         else:
             print(" ", end="")
     print("") 
