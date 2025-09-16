8c2d1b9f4bfc0e0215868187177c32df74930a59
yadavravi1610 <ravinderyadav1610@gmail.com>
2025-09-17 00:22:23 +0530

    Running loop in tuple

--- function.py
+++ function.py
@@ -1,10 +1,20 @@
-a = int(input())
-b = int(input())
+# a = int(input())
+# b = int(input())
 
-def sum(a,b):
-    print(a+b)
+# def sum(a,b):
+#     print(a+b)
 
-sum(a,b)    
+# sum(a,b)    
 
 # If we give more arguments than parameters it will show error instead of that in parameters we can take *num
 
+a = int(input())
+b = int(input())
+
+def sum(*numbers):
+    s = 0
+    for n in numbers:
+        s+=n
+    print(s)
+
+sum(a,b,8)   
