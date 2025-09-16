ee510abe1a7870210166b20340849a2c6376e320
yadavravi1610 <ravinderyadav1610@gmail.com>
2025-09-17 00:21:32 +0530

    Initializing and calling functions

---
+++ function.py
@@ -0,0 +1,10 @@
+a = int(input())
+b = int(input())
+
+def sum(a,b):
+    print(a+b)
+
+sum(a,b)    
+
+# If we give more arguments than parameters it will show error instead of that in parameters we can take *num
+
