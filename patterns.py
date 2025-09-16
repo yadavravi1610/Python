0ad04fe105322d066507468314eb064cbaabfa2c
yadavravi1610 <ravinderyadav1610@gmail.com>
2025-09-16 16:28:55 +0530

    Character pattern

--- patterns.py
+++ patterns.py
@@ -19,13 +19,23 @@
 #             print(" ", end="")
 #     print("") 
 
-n=4
-k=1
+# n=4
+# k=1
+# for i in range(1, n+1):
+#     for j in range(1, n+1):
+#         if j <= i:
+#             print(k, end="")
+#             k+=1 
+#         else:
+#             print(" ", end="")
+#     print("") 
+
+n=3
+
 for i in range(1, n+1):
     for j in range(1, n+1):
         if j <= i:
-            print(k, end="")
-            k+=1 
+            print(chr(ord('A')+ j-1), end="") 
         else:
             print(" ", end="")
     print("") 
