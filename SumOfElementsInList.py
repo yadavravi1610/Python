b8b30c390b6863e9e1555b44265396599f09417e
yadavravi1610 <ravinderyadav1610@gmail.com>
2025-09-18 01:18:21 +0530

    Sum of elements using sum function

--- SumOfElementsInList.py
+++ SumOfElementsInList.py
@@ -3,8 +3,8 @@ lists = input().split()
 for i in range(len(lists)):
     lists[i] = int(lists[i])
 
-sum = 0
-for i in range(len(lists)):
-    sum += lists[i]
+# sum = 0
+# for i in range(len(lists)):
+#     sum += lists[i]
 
-print(sum)
+print(sum(lists))
