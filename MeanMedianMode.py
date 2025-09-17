81a0392aee258e7fbcf70fcf1c76a00883e43943
yadavravi1610 <ravinderyadav1610@gmail.com>
2025-09-18 02:14:56 +0530

    Finding the median for the list

--- MeanMedianMode.py
+++ MeanMedianMode.py
@@ -11,3 +11,10 @@ for i in lists:
 mean = sum // len(lists)
 print(mean)
 
+if len(lists)%2 != 0:
+    median = len(lists)//2
+    print("Median for odd numbers = ", lists[median])
+if len(lists)%2 == 0:
+    median1 = len(lists)//2
+    # median2 = len(lists)+1//2
+    print("Median for even numbers =", lists[median1], ",", lists[median1+1])
