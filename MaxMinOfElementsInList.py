471fd7d318884bd4d6eb4d8f091fe987dd4d3740
yadavravi1610 <ravinderyadav1610@gmail.com>
2025-09-18 01:32:19 +0530

    Finding max and min using inbuilt functions in list

--- MaxMinOfElementsInList.py
+++ MaxMinOfElementsInList.py
@@ -4,13 +4,15 @@ lists = input().split()
 for i in range(len(lists)):
     lists[i] = int(lists[i])
 
-max = lists[0]
-min = lists[0]
+# max = lists[0]
+# min = lists[0]
 
-for i in range(len(lists)):              # for i in lists:
-    if min > lists[i]:                   #     if min > i               
-        min = lists[i]                   #         min = i
-    if max < lists[i]:                   #     if max <i
-        max = lists[i]                   #         max = i
+# for i in range(len(lists)):              # for i in lists:
+#     if min > lists[i]:                   #     if min > i               
+#         min = lists[i]                   #         min = i
+#     if max < lists[i]:                   #     if max <i
+#         max = lists[i]                   #         max = i
 
-print(max, min)
+# print(max, min) 
+
+print(max(lists), min(lists))
