764cc047ecaa2896e64bc898cc4e4afe2c8f093e
yadavravi1610 <ravinderyadav1610@gmail.com>
2025-09-18 00:18:46 +0530

    Adding elements in list using append

--- lists.py
+++ lists.py
@@ -1,5 +1,6 @@
 # list = [1,2,3]
 # print(list, type(list))
 
-list = [1,2,3]
-print(list[2]) // print(list[-1]) // print(list(len(list)- 1))
+# list = [1,2,3]
+# print(list[2]) // print(list[-1]) // print(list(len(list)- 1))
+
