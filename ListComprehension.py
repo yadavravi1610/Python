90b38e72eeec085988b20ef62d5568d949c01aec
yadavravi1610 <ravinderyadav1610@gmail.com>
2025-09-20 23:35:36 +0530

    List comprehension

---
+++ ListComprehension.py
@@ -0,0 +1,8 @@
+list =  [ele for ele in range(5)]
+
+#List comprehension offers a shorter syntax when you want to create a new list based on the values of an existing list
+
+#We can perform operation on first ele to alter the list like ele*2 that means it will multiple 2 with each
+#element in list
+
+print(list)
