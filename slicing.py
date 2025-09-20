4966257d9f5b0cb88a5baf53926b6385b238fb01
yadavravi1610 <ravinderyadav1610@gmail.com>
2025-09-20 23:25:48 +0530

    Negative step value

--- slicing.py
+++ slicing.py
@@ -1,2 +1,3 @@
 list = [10,20,30,40,50]
-print(list[::]) # default value is 0 : default value n: default value 1
+print(list[::-1]) # default value is 0 : default value n: default value 1
+# If we will use the step value in negative then it will reverse the list
