ceba4e92f63047dcebe4ff37e163417c1334b294
yadavravi1610 <ravinderyadav1610@gmail.com>
2025-09-20 23:37:59 +0530

    Taking input in list comprehension

--- ListComprehension.py
+++ ListComprehension.py
@@ -1,8 +1,12 @@
-list =  [ele for ele in range(5)]
+# list =  [ele for ele in range(5)]
 
 #List comprehension offers a shorter syntax when you want to create a new list based on the values of an existing list
 
 #We can perform operation on first ele to alter the list like ele*2 that means it will multiple 2 with each
 #element in list
 
-print(list)
+# print(list)
+
+list = [int(ele) for ele in input().split()] # Using this we can take input in list and default it is string
+#So for that we will typecase our element
+print(list) 
