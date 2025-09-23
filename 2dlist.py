db044de45181a43330e821ac5d812cb069cf8ff3
yadavravi1610 <ravinderyadav1610@gmail.com>
2025-09-23 22:57:28 +0530

    List in List

--- 2dlist.py
+++ 2dlist.py
@@ -1,8 +1,20 @@
-m=3
-n=3
+# m=3
+# n=3
+# list = []
+# for i in range(m):
+#     for j in range(n):
+#         list.extend(int(input().split()))
+
+# print(list)
+
+m = int(input())
+n = int(input())
+
 list = []
 for i in range(m):
+    tempList = input().split()
     for j in range(n):
-        list.extend(int(input().split()))
+        tempList[j] = int(tempList[j])
+    list.append(tempList)    
 
 print(list)
