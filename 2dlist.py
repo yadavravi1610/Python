599eaa667e905c1e0da0f020ba82159210a69565
yadavravi1610 <ravinderyadav1610@gmail.com>
2025-09-24 23:46:47 +0530

    Star pattern

--- 2dlist.py
+++ 2dlist.py
@@ -8,10 +8,21 @@
 # print(list)
 
 m = int(input())
-n = int(input())
+# n = int(input())
+
+# list = []
+# for i in range(m):
+#     tempList = [int(ele) for ele in input().split()] #shorthand of taking input for list in list
+#     list.append(tempList)
+# print(list)
 
 list = []
 for i in range(m):
-    tempList = [int(ele) for ele in input().split()] #shorthand of taking input for list in list
-    list.append(tempList)
-print(list)
+ #shorthand of taking input for list in list
+    list.append([int(ele) for ele in input().split()])
+
+for i in range(m):
+    for j in range(m):
+        if j<=i:
+            print(list[i][j], end=" ")
+    print()
