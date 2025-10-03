0fcdfa025c45bec031c41c05f19ef23dc5ebace5
yadavravi1610 <ravinderyadav1610@gmail.com>
2025-10-04 00:19:21 +0530

    You are given a number n. The number n can be negative or positive. If n is negative, print numbers from n to 0 by adding 1 to n in the neg function. If positive, print numbers from n-1 to 0 by subtracting 1 from n in the pos function.

--- forlooppractice.py
+++ forlooppractice.py
@@ -14,10 +14,33 @@
 # printInDecreasing(int(input()))       
 # 
 
-def printIncreasingPower(x):
-    n =1
-    while( n<=x):
-        if(n*n <= x):
-            print (n*n , end = " ")
+# def printIncreasingPower(x):
+#     n =1
+#     while( n<=x):
+#         if(n*n <= x):
+#             print (n*n , end = " ")
         
-        n += 1 
+#         n += 1 
+# printIncreasingPower(int(input()))
+
+def pos(n):
+    x=0
+    while (n>x):
+        n -= 1
+        print(n, end=" ")
+        
+    
+def neg(n):
+    x=0
+    while (n<=x):
+        print(n, end=" ")
+        n += 1
+
+n = int(input())
+
+if(n> 0):
+    pos(n)
+elif(n<0):
+    neg(n)
+elif(n == 0):
+    print("Already Zero")        
